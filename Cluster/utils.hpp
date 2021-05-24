473a0eb484093998607416f6eee530d4513b943b
Daria <68292792+dariakhaetskaya@users.noreply.github.com>
2021-05-24 11:02:10 +0700

    Create utils.hpp

---
+++ Cluster/utils.hpp
@@ -0,0 +1,24 @@
+//
+// Created by daria on 5/15/21.
+//
+
+#ifndef LAB5CLUSTER_UTILS_H
+#define LAB5CLUSTER_UTILS_H
+
+#define EXECUTOR_FINISHED_WORK -1
+#define SENDING_TASKS 656
+#define SENDING_TASK_COUNT 787
+#define NO_TASKS_TO_SHARE -565
+
+#define ANSI_RESET "\033[0m"
+#define ANSI_BLACK "\033[30m"
+#define ANSI_RED "\033[31m"
+#define ANSI_GREEN "\033[32m"
+#define ANSI_YELLOW "\033[33m"
+#define ANSI_BLUE "\033[34m"
+#define ANSI_PURPLE "\033[35m"
+#define ANSI_CYAN "\033[36m"
+#define ANSI_WHITE "\033[37m"
+#define ANSI_PURPLE_BG "\033[45m\033[30m"
+
+#endif //LAB5CLUSTER_UTILS_H
